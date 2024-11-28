522b032d7f5eb0fd9f4b8ea578263258f30072a6
Suchat027 <asd0981482773@gmail.com>
2024-11-28 11:03:00 +0700

    lab3

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-document.getElementById("messaga1").innerHTML = "hello Worid"
+document.getElementById("message1").innerHTML = "Hello World";
 
-//variable declaration
-let a = 5;
+    let a = 5;
     var b = "hello";
     const x = 10;
 
     console.log(b);
-
-    add(5 , 10);
 
     //function
     function add(aa,bb){
@@ -18,7 +15,7 @@ let a = 5;
 
 
     let result = add(5,10);
-
+    add(5,10);
     let add2 = function(aa,bb){
         let cc = aa + bb;
         console.log(cc);
@@ -27,56 +24,51 @@ let a = 5;
     let add3 = function (aa,bb){
         let cc = aa + bb;
         console.log(cc);
-        
     }
 
     //array
-    let arr = [1,2,3,4,5];
+    let arr = [1,"test",3,4,5];
     const car = [];
-    car[0] = "Toyota";
-    car[1] = "Homda";
-    const fruit = new Array ("Apple","Banana","Cherry");
-
+    car[0] = "bmw";
+    car[1] = "benz";
+    const fruit = new Array ("apple","banana","orange");
+    console.log(car[0]);
     console.log(car[1]);
-
-    let colors = [[1, 2, 3], "green", "blue"];
+    
+    let colors = [[1,2,3], "green","blue"];
     console.log(colors[0][2]);
 
     //array method
     fruit.push("Orange");
     console.log(fruit);
 
+    arr.map((item) => {
+        console.log(item);
+    });
+
     //object
     let person = {
-        firstName: "John",
-        lastName: "Doge",
-        age: 50,
-        child: ["Ann","Billy"],
-        fullname : function(){
-            return this.firstName + " " + this.lastName;
+        firtName: "John",
+        lastName: "Doe",
+        age: 25,
+        child: ["Anna","Billy"],
+        FullNmae: function(){
+            return this.firtName + " " + this.lastName
         }
     };
 
-    person.adderss = {
+    person.address = {
         street: "123 Main St",
-        city: "New York"
-    }
+        city : "New Yorke",
 
-    console.log(person.fullname());
+    };
+    console.log(person.FullNmae());
 
     //spread operator
-    const arrCombine = [...car,...fruit];
-    const arrCombine2 = [ar,fruit];
+    const arrCombine = [...car, ...fruit];
+    const arrCombine2 = [car, fruit];
     console.log(arrCombine);
     console.log(arrCombine2);
 
-    if (x == 10) {
-        let c = "test";
-        var d = "test2";
-        console.log("a is equal to b");
-    }
-    
-
-    arr.map((itim) =>{
-        console.log(itim);
-    });
+    //short term if else
+    let e = x==10 ? "yes" : "no";
